b6a063a69b1d7aaf8e99c35ebf6b7c9ad123a6eb
Petr Janik <peta.janik@email.cz>
2022-02-03 16:18:01 +0100

    Finish render and hydrate demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App({ ssr = false }: { ssr?: boolean }) {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(prev => prev + 1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{ssr ? 'SSR' : 'CLIENT'}</p>
+          <button onClick={increment}>Increment</button>
+          <p>{count}</p>
+        </header>
+      </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactDOMServer from 'react-dom/server';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+/* Observations */
+/*
+1) hydrate works even without previous content, but emits development error  
+'Warning: Expected server HTML to contain a matching <div> in <div>.'
+2) When contents differ (due to use of ssr prop), both hydrate and render
+emit development error
+'Warning: Text content did not match. Server: "SSR" Client: "CLIENT"'
+3) render emits development warning
+'Warning: render(): Calling ReactDOM.render() to hydrate server-rendered markup
+ will stop working in React v18. Replace the ReactDOM.render() call with ReactDOM.hydrate()
+ if you want React to attach to the server HTML.'
+ It can spot SSR content by its 'data-reactroot' attribute.
+ If you use 'renderToStaticMarkup' instead of 'renderToString',
+ the 'data-reactroot' attribute is not present and the warning is not emitted.
+ */
+
+const html = ReactDOMServer.renderToString(<App ssr />);
+console.log('HTML', html);
+
+const root = document.getElementById('root');
+root!.innerHTML = html;
+
+ReactDOM.hydrate(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
